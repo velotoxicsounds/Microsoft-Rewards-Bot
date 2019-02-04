e2a6841a82f192734224f349c7525234f60aa712
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2019-02-04 10:12:06 -0800

    Auto-Download chromedriver
    This is a quick modification to automatically download the most current chromedriver.. should eliminate lots of probs

--- ms_rewards.py
+++ ms_rewards.py
@@ -6,12 +6,15 @@
 # FIXME mobile version does not require re-sign in, but pc version does, why?
 # FIXME Known Cosmetic Issue - logged point total caps out at the point cost of the item on wishlist
 
-import os
 import argparse
 import json
-import time
-import random
 import logging
+import os
+import platform
+import random
+import re
+import time
+import zipfile
 from _datetime import datetime, timedelta
 
 import requests
@@ -20,12 +23,12 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException, TimeoutException, \
     ElementClickInterceptedException, ElementNotVisibleException, \
     ElementNotInteractableException, NoSuchElementException, UnexpectedAlertPresentException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 
 # URLs
 BING_SEARCH_URL = 'https://www.bing.com/search'
@@ -119,6 +122,40 @@ def get_login_info():
         return json.load(f)
 
 
+def download_driver(driver_path, system):
+    # determine latest chromedriver version
+    url = "https://sites.google.com/a/chromium.org/chromedriver"
+    r = requests.get(url)
+    latest_version = max([float("{}.{}".format(version[0], version[1]))
+                          for version in re.findall("ChromeDriver (\d+).(\d+)", r.text)])
+    if system == "Windows":
+        url = "https://chromedriver.storage.googleapis.com/{}/chromedriver_win32.zip".format(latest_version)
+    elif system == "Darwin":
+        url = "https://chromedriver.storage.googleapis.com/{}/chromedriver_mac64.zip".format(latest_version)
+    elif system == "Linux":
+        url = "https://chromedriver.storage.googleapis.com/{}/chromedriver_linux64.zip".format(latest_version)
+
+    response = requests.get(url, stream=True)
+    zip_file_path = os.path.join(os.path.dirname(driver_path), os.path.basename(url))
+    handle = open(zip_file_path, "wb")
+    for chunk in response.iter_content(chunk_size=512):
+        if chunk:  # filter out keep alive chunks
+            handle.write(chunk)
+    handle.close()
+    extracted_dir = os.path.splitext(zip_file_path)[0]
+    with zipfile.ZipFile(zip_file_path, "r") as zip_file:
+        zip_file.extractall(extracted_dir)
+    os.remove(zip_file_path)
+
+    driver = os.listdir(extracted_dir)[0]
+    os.rename(os.path.join(extracted_dir, driver), driver_path)
+    os.rmdir(extracted_dir)
+
+    os.chmod(driver_path, 0o755)
+    # way to note which chromedriver version is installed
+    open(os.path.join(os.path.dirname(driver_path), "{}.txt".format(latest_version)), "w").close()
+
+
 def browser_setup(headless_mode, user_agent):
     """
     Inits the chrome browser with headless setting and user agent
@@ -126,6 +163,15 @@ def browser_setup(headless_mode, user_agent):
     :param user_agent: String
     :return: webdriver obj
     """
+    os.makedirs('drivers', exist_ok=True)
+    path = os.path.join('drivers', 'chromedriver')
+    system = platform.system()
+    if system == "Windows":
+        if not path.endswith(".exe"):
+            path += ".exe"
+    if not os.path.exists(path):
+        download_driver(path, system)
+
     options = Options()
     if headless_mode:
         options.add_argument('--headless')
@@ -134,11 +180,7 @@ def browser_setup(headless_mode, user_agent):
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
-    "Windows, MacOS"
-    chrome_obj = webdriver.Chrome(chrome_options=options)
-
-    "Linux"
-    # chrome_obj = webdriver.Chrome(executable_path=r'/usr/local/bin/chromedriver', chrome_options=options)
+    chrome_obj = webdriver.Chrome(path, chrome_options=options)
 
     return chrome_obj
 
